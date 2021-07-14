8c81abd2142c1bdd0068d29547a8cbe2136a5c32
Queen Nnakwue <qnnakwue@gmail.com>
2021-07-14 15:52:59 +0100

    addidng new features

---
+++ ex2.js
@@ -0,0 +1,19 @@
+
+function firstName (firstName) {
+  var nameIntro = "My name is ";
+  // this is an inner function which as we will get to see, has access to the outer function's variables
+
+//this function lastName is a closure because it is a function inside another function
+   function lastName (lastName) {
+        return nameIntro + firstName + " " + lastName;
+    }
+    return lastName;
+}
+
+var fullName = firstName("Queen");
+//The firstName outer function has returned which we passed to this var fullName
+
+fullName ("Nnakwue");  // My name is Queen Nnakwue
+
+// The closure (lastName) is called after the outer function has returned above
+// Yet, the closure still has access to the outer function's //variables and parameter
